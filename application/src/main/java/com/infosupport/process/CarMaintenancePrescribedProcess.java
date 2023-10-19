50d9c24fc78a8f1d5875194fd9a4b373c2665dea
TimGe <timgerry.geerts@infosupport.com>
2023-10-19 13:20:25 +0200

    Handle processes async

--- application/src/main/java/com/infosupport/process/CarMaintenancePrescribedProcess.java
+++ application/src/main/java/com/infosupport/process/CarMaintenancePrescribedProcess.java
@@ -5,7 +5,10 @@ import com.infosupport.car.event.CarMaintenancePrescribed;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @AllArgsConstructor
@@ -13,7 +16,15 @@ public class CarMaintenancePrescribedProcess {
 
     public final ApplicationEventPublisher eventPublisher;
 
+    /*
+    * The transaction will have been committed already,
+    * but the transactional resources might still be active and accessible.
+    * As a consequence, any data access code triggered at this point will still “participate” in the original transaction,
+    * allowing to perform some cleanup (with no commit following anymore!)
+    * */
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @EventListener
+    @Async
     public void onCarMaintenanceRequired(CarMaintenancePrescribed carMaintenancePrescribed) {
         final var markCarForMaintenance = new MarkCarForMaintenance(carMaintenancePrescribed.licensePlate());
         eventPublisher.publishEvent(markCarForMaintenance);
